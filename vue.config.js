c9e3cd4e97595b57e2fc00fff426d354dc424bef
Rémy Marronnier <remy.marronnier@niji.fr>
2019-03-28 11:10:01 +0100

    githubpages config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    pluginOptions: {
-        webpackBundleAnalyzer: {
-            openAnalyzer: true
-        }
-    }
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/bafouille-pwa/'
+    : '/'
 };
