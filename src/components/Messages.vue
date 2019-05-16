532368a7c758bbf019cd13ae70b24ae84740c021
Rémy Marronnier <rmarronnier@gmail.com>
2019-05-17 01:52:25 +0200

    Now, we're talking

---
+++ src/components/Messages.vue
@@ -0,0 +1,14 @@
+<template>
+      <div>
+        <article v-for="message in messages" v-bind:key="message.id">{{ message.body }}</article>
+      </div>
+</template>
+
+<script>
+export default {
+    name : 'Messages',
+    props : {
+      messages : Object
+    }
+}
+</script>
--- src/views/Pairs.vue
+++ src/views/Pairs.vue
@@ -4,16 +4,27 @@
         v-for="pair in pairs"
         v-bind:key="pair.id"
       >
-        <article>{{ pair.users[0].body }}</article>
+        <article>
+          {{ pair.users[0].body }}
+          <Messages 
+  v-bind:messages="pair.messages">
+            </Messages>   
+          </article>
       </div>
   </section>
 </template>
 
 <script>
+import Messages from '@/components/Messages.vue'
+
 export default {
     name : 'Pairs',
      computed : {
        pairs : function(){ return this.$store.getters.pairs}
-     }
+     },
+     components: {
+    Messages
+  }
+
 }
 </script>
